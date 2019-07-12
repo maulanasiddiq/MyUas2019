9895c22b460f2f15c14ae1c1d941d6ec458b8857
Maulana Abdul Siddiq <siddiqmaulanaabdul@gmail.com>
2019-07-13 00:41:39 +0700

    Add UAS 2019

---
+++ app/src/main/java/com/maulanaabdulsiddiq/myuas2019/model/DataItem.java
@@ -0,0 +1,97 @@
+package com.maulanaabdulsiddiq.myuas2019.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class DataItem{
+
+	@SerializedName("nik")
+	private String nik;
+
+	@SerializedName("nama")
+	private String nama;
+
+	@SerializedName("updated_at")
+	private String updatedAt;
+
+	@SerializedName("kelas")
+	private String kelas;
+
+	@SerializedName("jam")
+	private String jam;
+
+	@SerializedName("created_at")
+	private String createdAt;
+
+	@SerializedName("id")
+	private int id;
+
+	public void setNik(String nik){
+		this.nik = nik;
+	}
+
+	public String getNik(){
+		return nik;
+	}
+
+	public void setNama(String nama){
+		this.nama = nama;
+	}
+
+	public String getNama(){
+		return nama;
+	}
+
+	public void setUpdatedAt(String updatedAt){
+		this.updatedAt = updatedAt;
+	}
+
+	public String getUpdatedAt(){
+		return updatedAt;
+	}
+
+	public void setKelas(String kelas){
+		this.kelas = kelas;
+	}
+
+	public String getKelas(){
+		return kelas;
+	}
+
+	public void setJam(String jam){
+		this.jam = jam;
+	}
+
+	public String getJam(){
+		return jam;
+	}
+
+	public void setCreatedAt(String createdAt){
+		this.createdAt = createdAt;
+	}
+
+	public String getCreatedAt(){
+		return createdAt;
+	}
+
+	public void setId(int id){
+		this.id = id;
+	}
+
+	public int getId(){
+		return id;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"DataItem{" + 
+			"nik = '" + nik + '\'' + 
+			",nama = '" + nama + '\'' + 
+			",updated_at = '" + updatedAt + '\'' + 
+			",kelas = '" + kelas + '\'' + 
+			",jam = '" + jam + '\'' + 
+			",created_at = '" + createdAt + '\'' + 
+			",id = '" + id + '\'' + 
+			"}";
+		}
+}
